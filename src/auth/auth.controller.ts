f5feefd57188b6d090e4e30b8d2db0c02442a103
Mayank Shakya <mayank.shakya@primathon.in>
2026-01-20 11:59:02 +0530

    login route implimented

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,21 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { RegisterUserDto } from './dto/RegisterUserDto';
 import bcrypt from 'bcrypt';
+import { AuthService } from './auth.service';
+import { LoginUserDto } from './dto/LoginUserDto';
 
 @Controller('auth')
 export class AuthController {
 
-   constructor(private readonly UserService: UserService){}
+   constructor(private readonly AuthService: AuthService){}
 
     @Post('register')
-    async register(@Body() RegisterUserDto:RegisterUserDto) {
+    async registerUser(@Body() RegisterUserDto:RegisterUserDto) {
+        return this.AuthService.registerUser(RegisterUserDto);
+    }
 
-        const hash=await bcrypt.hash(RegisterUserDto.password,10);
-        RegisterUserDto.password=hash;
-        return this.UserService.createUser(RegisterUserDto);
+    @Post('login')
+    async loginUser(@Body() loginUserDto:LoginUserDto) {
+        return this.AuthService.loginUser(loginUserDto);
     }
 }
--- src/auth/dto/RegisterUserDto.ts
+++ src/auth/dto/RegisterUserDto.ts
@@ -1,6 +1,15 @@
+import { IsEmail, IsString } from "class-validator";
+
 export class RegisterUserDto {
+    @IsString()
     fname: string;
+
+    @IsString()
     lname: string
+
+    @IsEmail()
     email: string;
+
+    @IsString()
     password: string;
 }
